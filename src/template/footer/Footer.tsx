1d58f0e235cba256d0018d3317fde3b0d7be781c
Justin <barbatos@argsea.com>
2024-01-20 04:53:40 -0500

    Add overscroll-behavior-y property to body element Update Footer component with user information and tech interests Add new Lighthouse5 component to Footer Add short bio and copyright information to Footer Add Lighthouse5-3 SVG file

--- src/template/footer/Footer.tsx
+++ src/template/footer/Footer.tsx
@@ -1,10 +1,15 @@
-import React from "react";
 import "./styles/footer.css";
 import iContacts from "../../interfaces/iContacts";
 import { ReactSVG } from "react-svg";
 import { Lighthouse } from "./Lighthouse";
+import iTechInterest from "../../interfaces/iTechInterest";
+import iUser from "../../interfaces/iUser";
+import { Lighthouse2 } from "./Lighthouse2";
+import { Lighthouse3 } from "./Lighthouse3";
+import { Lighthouse4 } from "./Lighthouse4";
+import { Lighthouse5 } from "./Lighthouse5";
 
-const Footer = ({ user }: { user: any }) => {
+const Footer = ({ user }: { user: iUser }) => {
   const currentYear = new Date().getFullYear();
   console.log(user);
   return (
@@ -24,14 +29,28 @@ const Footer = ({ user }: { user: any }) => {
           })}
         </div>
         <div id="footer-logo">
-          <a href="/">{<Lighthouse />}</a>
+          <a href="/">{<Lighthouse5 />}</a>
         </div>
+        <div id="footer-short-bio">
+          <h1>Justin Smith</h1>
+          <p>
+            As a versatile Full Stack Developer at a major news publisher, I specialize in building and integrating APIs, managing DevOps, and designing robust
+            system architectures. My work ensures seamless data flow and optimal system performance, contributing significantly to the delivery of timely and
+            reliable news to our audience.
+          </p>
+        </div>
+        <hr></hr>
         <div id="footer-legal">
-          <p>© {currentYear} argSea</p>
+          <p>© Copyright {currentYear}. Made by Justin Smith</p>
         </div>
-        {/* <div id="footer-admin-login">
-          <a href="/login">Admin</a>
-        </div> */}
+        <div id="footer-tech-interests">
+          <span id="footer-tech-interests-title">Current Interests: </span>
+          <div id="footer-tech-interests-text">
+            {user.techInterests.map((interest: iTechInterest) => {
+              return <div className="footer-tech-interest">{interest.name}</div>;
+            })}
+          </div>
+        </div>
       </div>
     </footer>
   );
